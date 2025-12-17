ee7201ef612314d2cd1b36b2ec22b5186b562525
AnushaKallam3003 <kallamanusha67@gmail.com>
2025-12-17 20:29:01 +0530

    Roboshop Project

---
+++ resources/main.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "web" {
+  ami = "ami-09c813fb71547fc4f"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "test"
+  }
+}
+resource "aws_route53_record" "www" {
+  zone_id = "Z05390891F75TBI7415YG"
+  name    = "test"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.web.private_ip]
+}
